8c663baa5d5c38f8616dd0e5354bbe4a6971f17a
Yissel51 <163574365+Yissel51@users.noreply.github.com>
2024-07-30 22:33:21 -0400

    Add files via upload
    Nuevos cambios al blog

---
+++ Redireccion.js
@@ -0,0 +1,9 @@
+function redireccion(){
+    location.href="Informacion1.html"
+}
+function redireccion2(){
+    location.href="Informacion2.html"
+}
+function redireccion3(){
+    location.href="Informacion3.html"
+}
